86e85fcaa38d5fc6609d9ae04ac25c24c575b4f4
OkanZengin <44057708+OkanZengin@users.noreply.github.com>
2023-08-21 10:32:38 +0300

    added part7 - hashmaps.java, corrected a couple of lines

--- src/part_seven/Collections_one.java
+++ src/part_seven/Collections_one.java
@@ -4,11 +4,11 @@ package part_seven;
 public class Collections_one {
 
 	public static void main(String[] args) {
-//		Collections_Lists method = new Collections_Lists();
-//		method.list_strings_method();
-//		method.list_integers_method();
-//		method.listvehicles();
-//		method.listtvehicles();
+		Collections_Lists method = new Collections_Lists();
+		method.list_strings_method();
+		method.list_integers_method();
+		method.listvehicles();
+		method.listtvehicles();
 		HashMaps newhash = new HashMaps();
 		newhash.hashmapping();
 		newhash.linkedhashmapping();
